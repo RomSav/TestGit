dfa218137302389c154bdd3a08b702ecf3a86a67
RomSav <freeon84@gmail.com>
2014-03-19 21:20:08 +0200

    second commit

--- TestGit/src/main/java/Main.java
+++ TestGit/src/main/java/Main.java
@@ -3,6 +3,16 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test git");
+
+        int a = 2;
+        int b = 3;
+        Test t = new Test();
+        t.sum(a, b);
+    }
+}
+
+class Test {
+    public void sum(int a, int b) {
+        System.out.println(a + b);
     }
 }
